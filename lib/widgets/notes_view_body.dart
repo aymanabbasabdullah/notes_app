0befd49147c67bb614631415f9d630d5fe58c32a
aymanajlan <ayman345abbas@gmail.com>
2024-08-05 11:36:14 +0300

    creat Notes list view

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,15 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import '../views/custom_note_item.dart';
+import 'custom_note_item.dart';
 import 'custom_app_bar.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+    ['note', 'note'];
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           SizedBox(
@@ -18,8 +22,8 @@ class NotesViewBody extends StatelessWidget {
           CustomAppBar(),
           SizedBox(
             height: 20,
-          ),
-          NoteItem()
+          )  ,
+          Expanded(child: NotesListView()),
         ],
       ),
     );
