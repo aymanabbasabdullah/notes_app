f73c5e3e0398cf4c49f09ed2cfe4c567b2c2f538
aymanajlan <ayman345abbas@gmail.com>
2024-08-09 12:17:59 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp/cubits/add_note_cubit.dart';
+import 'package:notesapp/simple_bloc_observer.dart';
 import 'package:notesapp/views/edit_note_view.dart';
 import 'package:notesapp/views/notes_view.dart';
-
 import 'constants.dart';
 import 'models/note_model.dart';
 
 void main() async{
   await Hive.initFlutter();
+  Bloc.observer =SimpleBlocObserver();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
@@ -8,16 +6,15 @@ import 'add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+      padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
       child: SingleChildScrollView(
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             if(state is AddNoteFailure){
-              print('failied ${state.errMessage}');
+              print(' ailied ${state.errMessage}');
             }
             if(state is AddNoteSuccess){
               Navigator.pop(context);
